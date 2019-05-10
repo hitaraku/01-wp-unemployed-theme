7b308ca73df61f1b3a17001212405254179fbd01
Hitaraku <n.hitaraku@gmail.com>
2019-05-11 08:59:37 +1200

    change-header

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- vendor -->
-		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<!-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
 
 		<!-- own -->
 		<link rel="stylesheet" href="<? bloginfo('stylesheet_url');?>">
@@ -29,7 +29,7 @@
 		<?php endif; ?>
 	</head>
 	
-	<body>
+	<body class="customize-support">
 		<header>
 			<div id="mobile-menu"><ion-icon name="menu" style="width:30px;height:30px;"></ion-icon></div>
 			<div id="header-title">職業、<span id="impact-mu"><a href="/">無</a></span><span id="header-title-last">職</span>。</div>		
